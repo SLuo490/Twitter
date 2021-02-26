2f73fbac17a7fafa5479b19e19bd06f7d175fad5
Shi Tao Luo <sluo5991@gmail.com>
2021-02-26 13:15:50 -0500

    Implemented log out

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Shi Tao Luo on 2/26/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+    
+        //Call twitter api to log out
+        TwitterAPICaller.client?.logout()
+        
+        //goes back to login screen
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -29,6 +29,7 @@ class LoginViewController: UIViewController {
             print("Error: Could Not Login")
         })
         
+        
     }
     
     
